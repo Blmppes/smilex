f92748e360a518a174754889ac30e0e4a40efafa
Blmppes <43724804+Blmppes@users.noreply.github.com>
2025-07-02 10:36:36 +0700

    Update extract_all.py

--- extract_all.py
+++ extract_all.py
@@ -18,57 +18,67 @@ def extract_all_features(input_path, output_path, normalization='False', fill_na
   atom_df = pd.DataFrame(local_batch.f_atoms_pca).add_prefix("A_")
   bond_df = pd.DataFrame(local_batch.f_bonds_pca).add_prefix("B_")
 
-#seed(s)
-smilesF = pd.read_csv('/kaggle/input/a-lot-of-compounds/SMILES_200Features.csv')
+  print("Extracting global features...")
+  smilesF = process_global_feature_extraction(
+        input_path=input_path,
+        output_path=None,  # no save
+        output_type='csv',
+        normalization=normalization,
+        fill_nan=fill_nan
+    )
 
-sheader = []
-
-smilesF = smilesF.fillna(0)
-
-#smilesF2 = smilesF.transpose()
-sheader = list(smilesF.columns.values)
-scaler = preprocessing.StandardScaler()
-scaled = scaler.fit_transform(smilesF)
-scaled_df = pd.DataFrame(scaled, columns = sheader)
-#scaled_df = scaled_df.transpose()
-
-atom_features = atom_features.add_prefix('A_')
-
-bond_features = bond_features.add_prefix('B_')
-
-for i in range (0,12):
-  i = str(i)
-  column = 'B_'+i
-  b1 = bond_features[column]
+  sheader = []
   
-  name = column
-  i = int(i)
-  atom_features.insert(i,name,b1)
+  smilesF = smilesF.fillna(0)
 
-header_smilesf = []
-header_smilesf = list(smilesF.columns.values)
+  #smilesF2 = smilesF.transpose()
+  sheader = list(smilesF.columns.values)
+  scaler = preprocessing.StandardScaler()
+  scaled = scaler.fit_transform(smilesF)
+  scaled_df = pd.DataFrame(scaled, columns = sheader)
+  #scaled_df = scaled_df.transpose()
 
-#tf.random.set_seed(s)
-#seed(s)
-second_pca = PCA(n_components = 50) 
-data_atom_pca = second_pca.fit_transform(atom_features)
-
-pcaNames = []
-for p in range(1,51):
-  pc = str(p)
-  pca = 'PCA'+pc
-  pcaNames.append(pca)
-
-
-data_atom_pca = pd.DataFrame(data=data_atom_pca, columns=pcaNames)
-
-j = 0
-for col in pcaNames:
-  col_data = data_atom_pca[col]
-  scaled_df.insert(j,col,col_data)
+  atom_features = atom_features.add_prefix('A_')
   
-  j = j+1
+  bond_features = bond_features.add_prefix('B_')
+
+  for i in range (0,12):
+    i = str(i)
+    column = 'B_'+i
+    b1 = bond_features[column]
+    
+    name = column
+    i = int(i)
+    atom_features.insert(i,name,b1)
+  
+  header_smilesf = []
+  header_smilesf = list(smilesF.columns.values)
+
+  #tf.random.set_seed(s)
+  #seed(s)
+  second_pca = PCA(n_components = 50) 
+  data_atom_pca = second_pca.fit_transform(atom_features)
+  
+  pcaNames = []
+  for p in range(1,51):
+    pc = str(p)
+    pca = 'PCA'+pc
+    pcaNames.append(pca)
+  
+  
+  data_atom_pca = pd.DataFrame(data=data_atom_pca, columns=pcaNames)
+  
+  j = 0
+  for col in pcaNames:
+    col_data = data_atom_pca[col]
+    scaled_df.insert(j,col,col_data)
+    
+    j = j+1
 
 
-sel = VarianceThreshold(0)
-cleaned = sel.fit_transform(scaled_df)
+  sel = VarianceThreshold(0)
+  cleaned = sel.fit_transform(scaled_df)
+  cleaned = pd.DataFrame(cleaned)
+  cleaned.to_csv(output_path + "_all.csv", index=False)
+  print(f"All features extracted and saved to {output_path + '_all.csv'}")
+  return cleaned
